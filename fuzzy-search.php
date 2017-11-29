a98e222ce7e9199fb8fbce9f3369f0fd213be85e
Pedro Borges <oi@pedroborg.es>
2017-11-29 01:42:56 -0200

    Extract functions to a class and rename plugin

---
+++ fuzzy-search.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Kirby Fuzzy Search Plugin
+ *
+ * @version   1.0.0-beta
+ * @author    Pedro Borges <oi@pedroborg.es>
+ * @copyright Pedro Borges <oi@pedroborg.es>
+ * @link      https://github.com/pedroborges/kirby-fuzzy-search
+ * @license   MIT
+ */
+
+// Load Composer dependencies
+require __DIR__.DS.'vendor'.DS.'autoload.php';
+
+use PedroBorges\FuzzySearch\FuzzySearch;
+
+if (! function_exists('fuzzySearch')) {
+    /**
+     * Perform fuzzy-search on an array of data.
+     *
+     * @param mixed $data
+     * @param string $query
+     * @param array $fields
+     * @return array
+     */
+    function fuzzySearch($data, string $query, $fields = []) {
+        $fuzzy = new FuzzySearch($data, $fields);
+
+        return $fuzzy->search($query);
+    }
+}
+
+$kirby->set('field::method', 'fuzzySearch', function(Field $field, string $query, $fields = []) {
+    $results = fuzzySearch($field, $query, $fields);
+
+    // Return result as Field to allow
+    // for chaining of field methods
+    return new Field(
+        $field->page(),
+        $field->key(),
+        Yaml::encode($results)
+    );
+});
+
+$kirby->set('pages::method', 'fuzzySearch', function(Pages $pages, string $query, $fields = []) {
+    $results = fuzzySearch($pages, $query, $fields);
+
+    // Retrieve pages IDs from results
+    // to rebuild Pages collection
+    $pageIDs = [];
+    foreach ($results as $page) {
+        $pageIDs[] = $page['id'];
+    }
+
+    return $pages->filterBy('id', 'in', $pageIDs);
+});
---
+++ src/FuzzySearch.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace PedroBorges\FuzzySearch;
+
+use InvalidArgumentException;
+use Field;
+use Pages;
+use Str;
+use kevinfiol\fuzzget\Fuzz;
+
+class FuzzySearch
+{
+    protected $data = [];
+    protected $included = [];
+    protected $ignored = [];
+
+    public function __construct($data, $fields = [])
+    {
+        $this->data = $data;
+        $this->setFields($fields);
+    }
+
+    public function search(string $query)
+    {
+        $fuzz = new Fuzz($this->parsedData(), 10, 1, true);
+
+        return $fuzz->search($query, 3);
+    }
+
+    protected function parsedData()
+    {
+        $data = $this->data;
+
+        if (is_a($data, 'Collection')) {
+            return $this->pagesToArray($data);
+        }
+
+        if (is_a($data, 'Field')) {
+            return $this->fieldToArray($data);
+        }
+
+        if (! is_array($data)) {
+            throw new InvalidArgumentException(
+                'Provided $data must be an array, instance of Pages or Field.'
+            );
+        }
+
+        return array_map(function($content) {
+            return $this->filterContent($content);
+        }, $data);
+    }
+
+    protected function pagesToArray(Pages $pages)
+    {
+        $data = array_map(function($page) {
+            $content = $page->content()->toArray();
+            $content = $this->filterContent($content);
+
+            // Fields not present in the content array
+            // are considered computed fields and will be
+            // treated as custom page method or model method
+            $computed = array_diff(
+                $this->included,
+                $page->content()->fields()
+            );
+
+            if (count($computed)) {
+                // Remove wildcard from computed fields
+                if (isset($computed[0]) && $computed[0] === '*') {
+                    unset($computed[0]);
+                }
+
+                foreach ($computed as $field) {
+                    $content[$field] = (string) $page->{$field}();
+                }
+            }
+
+            $content['id'] = $page->id();
+
+            return $content;
+        }, $pages->data);
+
+        return array_values($data);
+    }
+
+    protected function fieldToArray(Field $field)
+    {
+        return array_map(function($content) {
+            return $this->filterContent($content);
+        }, $field->yaml());
+    }
+
+    protected function filterContent(array $content)
+    {
+        // Do not filter content if wildcard
+        // was passed as first item
+        if (count($this->included) && $this->included[0] !== '*') {
+            $content = array_filter($content, function($field) {
+                return in_array($field, $this->included);
+            }, ARRAY_FILTER_USE_KEY);
+        }
+
+        if (count($this->ignored)) {
+            $content = array_filter($content, function($field) {
+                return ! in_array($field, $this->ignored);
+            }, ARRAY_FILTER_USE_KEY);
+        }
+
+        return $content;
+    }
+
+    protected function setFields($fields)
+    {
+        if (is_string($fields)) {
+            $fields = Str::split($fields, '|');
+        }
+
+        $this->included = isset($fields['included'])
+            ? $fields['included']
+            : $this->fieldsToInclude($fields);
+
+        $this->ignored = isset($fields['ignored'])
+            ? $fields['ignored']
+            : $this->fieldsToIgnore($fields);
+    }
+
+    protected function fieldsToInclude(array $fields = [])
+    {
+        return array_filter($fields, function($field) {
+            return ! is_array($field) && ! Str::startsWith($field, '-');
+        });
+    }
+
+    protected function fieldsToIgnore(array $fields = [])
+    {
+        $fields = array_filter($fields, function($field) {
+            return ! is_array($field) && Str::startsWith($field, '-');
+        });
+
+
+        // Remove dash and return ignored fields
+        return array_map(function($field) {
+            return substr($field, 1);
+        }, $fields);
+    }
+}
